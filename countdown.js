e580b0d3dba66e08b572b0826f586b1673324484
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2023-03-02 22:22:42 +0530

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -9,7 +9,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "10/21/", // Set your day and month here
+        dayMonth = "03/03/", // Set your day and month here
         birthday = dayMonth + yyyy;
 
     today = mm + "/" + dd + "/" + yyyy;
